0fa8d84424dc4bd9c08caab23e6ad86734c3c51b
Laszlo-Roovers <64090750+Laszlo-Roovers@users.noreply.github.com>
2024-06-13 16:15:50 +0200

    Modularization of the upsampling stage for the vision transformer

--- VisionTransformer.py
+++ VisionTransformer.py
@@ -36,7 +36,7 @@ class PatchEmbedding(nn.Module):
         Returns
         -------
         torch.Tensor
-            Patch embeddings of shape `(B, num_patches, dim)`.
+            Patch embeddings of shape `(Batch, num_patches, dim)`.
         """
         B, C, H, W = x.shape
         patches = x.unfold(2, self.patch_size, self.patch_size).unfold(3, self.patch_size, self.patch_size)
@@ -44,6 +44,40 @@ class PatchEmbedding(nn.Module):
         patches = self.projection(patches)
         patches += self.position_embeddings
         return patches
+    
+class Upsampler(nn.Module):
+    """Upsample feature maps to to match the original input dimensions.
+
+    Parameters
+    ----------
+    dim : int
+        Dimensionality of the feature maps.
+    patch_size : int
+        Size of a single patch.
+    """
+    def __init__(self, dim, patch_size):
+        super(Upsampler, self).__init__()
+        self.upsample = nn.Sequential(
+            nn.ConvTranspose2d(dim, dim, kernel_size=patch_size, stride=patch_size),
+            nn.ReLU(inplace=True),
+            nn.ConvTranspose2d(dim, 1, kernel_size=1)
+        )
+    
+    def forward(self, x):
+        """Calculate a forward pass.
+
+        Parameters
+        ----------
+        x : torch.Tensor
+            Input tensor of shape `(Batch, dim, Height/patch_size, Width/patch_size).
+        
+        Returns
+        -------
+        torch.Tensor
+            Upsampled tensor of shape `(Batch, 1, Height, Width)`        
+        """
+        return self.upsample(x)
+
 
 
 class VisionTransformer(nn.Module):
@@ -78,11 +112,7 @@ class VisionTransformer(nn.Module):
         self.dim = dim
 
         # Upsampling to get back to the original dimensions.
-        self.upsample = nn.Sequential(
-            nn.ConvTranspose2d(dim, dim, kernel_size=patch_size, stride=patch_size),
-            nn.ReLU(inplace=True),
-            nn.ConvTranspose2d(dim, 1, kernel_size=1)
-        )
+        self.upsample = Upsampler(dim, patch_size)
 
     def forward(self, x):
         """Calculate a forward pass.
@@ -110,9 +140,20 @@ class VisionTransformer(nn.Module):
         return output
 
 
-
 model = VisionTransformer(img_size=256, patch_size=16, dim=768, depth=12, heads=12, mlp_dim=3072)
 test_input = torch.randn((1, 2, 256, 256))
 pred = model(test_input)
 print(pred.shape)
 
+from torchview import draw_graph
+architecture = 'ViT'
+model_graph = draw_graph(
+    model,
+    input_size=(1, 2, 256, 256),
+    graph_dir="TB",
+    roll=True,
+    expand_nested=True,
+    graph_name=f"self_{architecture}",
+    save_graph=True,
+    filename=f"self_{architecture}",
+)
